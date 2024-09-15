4d9a24577d6d2473b8a8128ce123cd4aef9d3cd0
dz1230 <dz123079@gmail.com>
2024-09-15 15:51:58 +0200

    added scene view

--- src/component.rs
+++ src/component.rs
@@ -1,15 +1,41 @@
-use std::sync::{Once, atomic::{AtomicUsize, Ordering}};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::any::TypeId;
+use std::collections::HashMap;
+use std::sync::Mutex;
 
-static TYPE_ID_COUNTER: AtomicUsize = AtomicUsize::new(0);
+lazy_static::lazy_static! {
+    static ref TYPE_ID_COUNTER: AtomicUsize = AtomicUsize::new(0);
+    static ref TYPE_ID_MAP: Mutex<HashMap<TypeId, usize>> = Mutex::new(HashMap::new());
+}
 
-pub fn get_type_id<T>() -> usize {
-    static TYPE_ID: AtomicUsize = AtomicUsize::new(usize::MAX);
-    static COMPONENT_INIT: Once = Once::new();
+pub fn get_type_id<T: 'static>() -> usize {
+    let type_id = TypeId::of::<T>();
 
-    COMPONENT_INIT.call_once(|| {
-        let new_id = TYPE_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
-        TYPE_ID.store(new_id, Ordering::Relaxed);
-    });
+    let mut map = TYPE_ID_MAP.lock().unwrap();
+    *map.entry(type_id).or_insert_with(|| TYPE_ID_COUNTER.fetch_add(1, Ordering::Relaxed))
+}
 
-    TYPE_ID.load(Ordering::Relaxed)
+// Collects component type IDs
+pub trait RequireComponents {
+    fn required_component_ids() -> Vec<usize>;
+}
+
+// Recursion base case
+impl RequireComponents for () {
+    fn required_component_ids() -> Vec<usize> {
+        Vec::new()
+    }
+}
+
+// Recursion case
+impl<Head, Tail> RequireComponents for (Head, Tail)
+where
+    Head: 'static,
+    Tail: RequireComponents,
+{
+    fn required_component_ids() -> Vec<usize> {
+        let mut ids = vec![get_type_id::<Head>()];
+        ids.extend(Tail::required_component_ids());
+        ids
+    }
 }
--- src/scene.rs
+++ src/scene.rs
@@ -1,7 +1,9 @@
+use std::{
+    any::Any,
+    collections::{HashMap, HashSet},
+};
 
-use std::{any::Any, collections::{HashMap, HashSet}};
-
-use crate::{component::get_type_id, entity::Entity, error::RecsError, pool::Pool};
+use crate::{component::{get_type_id, RequireComponents}, entity::Entity, error::RecsError, pool::Pool};
 
 pub struct EntityDescription {
     entity: Entity,
@@ -12,7 +14,7 @@ impl EntityDescription {
     pub fn new(entity: Entity) -> Self {
         EntityDescription {
             entity,
-            components: HashSet::new()
+            components: HashSet::new(),
         }
     }
 
@@ -25,6 +27,10 @@ impl EntityDescription {
     pub fn check_validity(&self, entity: Entity) -> bool {
         self.entity == entity && self.entity.is_valid()
     }
+
+    pub fn has_component_with_type_id(&self, type_id: usize) -> bool {
+        self.components.contains(&type_id)
+    }
 }
 
 pub struct Scene {
@@ -33,7 +39,7 @@ pub struct Scene {
     /// List of entities in the scene
     entities: Vec<EntityDescription>,
     /// Map of component pools keyed by type ID
-    pools: HashMap<usize, Box<dyn Any>>
+    pools: HashMap<usize, Box<dyn Any>>,
 }
 
 impl Scene {
@@ -41,7 +47,7 @@ impl Scene {
         Scene {
             free_list: Vec::new(),
             entities: Vec::new(),
-            pools: HashMap::new()
+            pools: HashMap::new(),
         }
     }
 
@@ -67,16 +73,24 @@ impl Scene {
         self.free_list.push(index);
     }
 
-    pub fn assign<T: 'static>(&mut self, entity: Entity, new_component: T) -> Result<&mut T, RecsError> {
+    pub fn assign<T: 'static>(
+        &mut self,
+        entity: Entity,
+        new_component: T,
+    ) -> Result<&mut T, RecsError> {
         let entity_description = self.get_valid_entity_description_mut(entity)?;
         entity_description.components.insert(get_type_id::<T>());
         let pool = self.get_or_create_pool::<T>()?;
         Ok(pool.assign(entity, new_component))
     }
 
-    pub fn assign_default<T: Default + 'static>(&mut self, entity: Entity) -> Result<&mut T, RecsError> {
+    pub fn assign_default<T: Default + 'static>(
+        &mut self,
+        entity: Entity,
+    ) -> Result<&mut T, RecsError> {
         let entity_description = self.get_valid_entity_description_mut(entity)?;
         entity_description.components.insert(get_type_id::<T>());
+        println!("{:?}", entity_description.components);
         let pool = self.get_or_create_pool::<T>()?;
         Ok(pool.assign_default(entity))
     }
@@ -84,29 +98,41 @@ impl Scene {
     pub fn remove<T: 'static>(&mut self, entity: Entity) -> Result<(), RecsError> {
         self.get_valid_entity_description_mut(entity)
             .map(|ed| ed.components.remove(&get_type_id::<T>()))?;
-        self.get_pool_if_exists_mut::<T>()
-            .map(|p| p.free(entity));
+        self.get_pool_if_exists_mut::<T>().map(|p| p.free(entity));
         Ok(())
     }
 
     pub fn get<T: 'static>(&self, entity: Entity) -> Result<Option<&T>, RecsError> {
         self.assert_entity_valid(entity)?;
-        Ok(self.get_pool_if_exists::<T>().map_or(None, |p| p.get(entity)))
+        Ok(self
+            .get_pool_if_exists::<T>()
+            .map_or(None, |p| p.get(entity)))
     }
 
     pub fn get_mut<T: 'static>(&mut self, entity: Entity) -> Result<Option<&mut T>, RecsError> {
         self.assert_entity_valid(entity)?;
-        Ok(self.get_pool_if_exists_mut::<T>().map_or(None, |p| p.get_mut(entity)))
+        Ok(self
+            .get_pool_if_exists_mut::<T>()
+            .map_or(None, |p| p.get_mut(entity)))
+    }
+
+    pub fn view<'a, T: RequireComponents>(&'a self) -> SceneView<'a> {
+        SceneView::new(self, T::required_component_ids())
     }
 
     fn assert_entity_valid(&self, entity: Entity) -> Result<(), RecsError> {
-        self.entities.get(entity.index() as usize)
+        self.entities
+            .get(entity.index() as usize)
             .filter(|ed| ed.check_validity(entity))
             .map_or(Err(RecsError::InvalidEntityError), |_| Ok(()))
     }
 
-    fn get_valid_entity_description_mut(&mut self, entity: Entity) -> Result<&mut EntityDescription, RecsError> {
-        self.entities.get_mut(entity.index() as usize)
+    fn get_valid_entity_description_mut(
+        &mut self,
+        entity: Entity,
+    ) -> Result<&mut EntityDescription, RecsError> {
+        self.entities
+            .get_mut(entity.index() as usize)
             .filter(|ed| ed.check_validity(entity))
             .map_or(Err(RecsError::InvalidEntityError), Ok)
     }
@@ -121,12 +147,54 @@ impl Scene {
     }
 
     fn get_pool_if_exists<T: 'static>(&self) -> Option<&Pool<T>> {
-        self.pools.get(&get_type_id::<T>())
+        self.pools
+            .get(&get_type_id::<T>())
             .and_then(|p| p.downcast_ref::<Pool<T>>())
     }
 
     fn get_pool_if_exists_mut<T: 'static>(&mut self) -> Option<&mut Pool<T>> {
-        self.pools.get_mut(&get_type_id::<T>())
+        self.pools
+            .get_mut(&get_type_id::<T>())
             .and_then(|p| p.downcast_mut::<Pool<T>>())
+    }
+}
+
+pub struct SceneView<'a> {
+    scene: &'a Scene,
+    required_components: Vec<usize>,
+    index: usize,
+}
+
+impl<'a> SceneView<'a> {
+    pub fn new(scene: &'a Scene, required_components: Vec<usize>) -> Self {
+        SceneView {
+            scene,
+            required_components,
+            index: 0,
+        }
+    }
+
+    fn entity_has_required_components(&self, entity_description: &EntityDescription) -> bool {
+        self.required_components
+            .iter()
+            .all(|&id| entity_description.has_component_with_type_id(id))
+    }
+}
+
+impl<'a> Iterator for SceneView<'a> {
+    type Item = Entity;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.index < self.scene.entities.len() {
+            let entity_description = &self.scene.entities[self.index];
+            self.index += 1;
+
+            if self.entity_has_required_components(entity_description)
+                && entity_description.entity.is_valid()
+            {
+                return Some(entity_description.entity);
+            }
+        }
+        None
     }
 }
